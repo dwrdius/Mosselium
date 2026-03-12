198938bf416bc0a02ae8f96934d9fa32cd431134
Edward Shao <e6shao@uwaterloo.ca>
2026-03-11 23:54:58 -0400

    Allow recursive flagging to-pdf converter to work on .cpp files too

--- bashUtils/floss/recursiveCcToPdf.sh
+++ bashUtils/floss/recursiveCcToPdf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for file in $( find -name "*.cc" -type f ); do
+for file in $( find -name "*.{cc,cpp}" -type f ); do
     directory=$(dirname "${file}")
     cd "${directory}/.."
     pwd
     parent=$(basename "${directory}")
     file=$(basename "${file}")
-    output=$(echo "$file" | sed 's/\.cc$/.pdf/')
+    output=$(echo "$file" | sed -e 's/\.cc$/.pdf/' -e 's/\.cpp$/.pdf/')
     output="${parent}/${output}"
     echo "${output}"
     enscript -Ecpp --color --line-numbers=1 -o - "${parent}/${file}" | ps2pdf - "${output}"
